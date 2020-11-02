f0a870b634f5f4ae46d840dcee599446a701c07c
Nils Martel <nils.martel@graphmasters.net>
2020-11-02 16:03:38 +0100

    Start writing code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::{cmp::Eq, collections::HashMap, hash::Hash};
+
+pub struct GraphArena<T>
+where
+    T: Sized + Hash + Eq,
+{
+    data: Vec<T>,
+    mapping: HashMap<T, usize>,
+}
+
+impl<T> GraphArena<T>
+where
+    T: Sized + Hash + Eq,
+{
+    pub fn new() -> Self {
+        GraphArena {
+            data: Vec::new(),
+            mapping: HashMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, item: T) -> usize {
+        if let Some(key) = self.mapping.get(&item) {
+            return *key;
+        }
+
+        let index = self.data.len();
+        self.data.push(item);
+        self.mapping.insert(item, index);
+        index
     }
 }
