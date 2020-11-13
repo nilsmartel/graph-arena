cd964ec870c05ff8a967929d4fd4dde148a29618
Nils Martel <nils.martel@graphmasters.net>
2020-11-13 15:29:45 +0100

    Implement functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
-use std::{cmp::Eq, collections::HashMap, hash::Hash};
+use std::{
+    cmp::Eq,
+    collections::{hash_map::DefaultHasher, HashMap},
+    hash::{Hash, Hasher},
+};
 
 pub struct GraphArena<T>
 where
     T: Sized + Hash + Eq,
 {
     data: Vec<T>,
-    mapping: HashMap<T, usize>,
+    mapping: HashMap<u64, usize>,
 }
 
 impl<T> GraphArena<T>
@@ -19,14 +23,48 @@ where
         }
     }
 
+    pub fn get(&self, key: usize) -> &T {
+        &self.data[key]
+    }
+
     pub fn insert(&mut self, item: T) -> usize {
-        if let Some(key) = self.mapping.get(&item) {
+        let hashsum = hash(&item);
+
+        if let Some(key) = self.mapping.get(&hashsum) {
+            // perfrom EQ test im data[key] and use new hasher, if it fails
+            // if self.data[*key] != item {
+            //     self.rehash();
+            //     return self.insert(item);
+            // }
+
             return *key;
         }
 
         let index = self.data.len();
         self.data.push(item);
-        self.mapping.insert(item, index);
+        self.mapping.insert(hashsum, index);
         index
+    }
+}
+
+fn hash<T>(item: &T) -> u64
+where
+    T: Hash,
+{
+    let mut hasher = DefaultHasher::new();
+    item.hash(&mut hasher);
+    hasher.finish()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_hash() {
+        let obj = (123, 456);
+        let h1 = hash(&obj);
+        let h2 = hash(&obj);
+        assert_eq!(h1, h2);
     }
 }
